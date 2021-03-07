57d0d6c018e5a0e4771af3aa627dc958a62b97e2
aboldyrev <andreybold@bk.ru>
2021-03-07 18:53:53 +0300

    Изменена папка для компиляции

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ const path = require('path')
  */
 module.exports = {
 	productionSourceMap: false,
-	outputDir: 'public',
+	outputDir: 'docs',
 	configureWebpack: {
 		plugins: [
 			new copyWebpackPlugin({
